0d01c16bfd699f2f19c81ad161623ec255670e38
Rodrigo Oliveira <rodrigo.oliveira@sage.com>
2018-01-19 14:14:49 +0000

    Add challenge

---
+++ challenge.php
@@ -0,0 +1,54 @@
+<?php
+
+	class NumberDivider{
+
+		private $divider;
+		private $message;
+
+		function __construct($divider, $message){
+			$this->divider = $divider;
+			$this->message = $message;
+		}
+
+		public function validation($number){
+			return ($number % $this->divider == 0 ? $this->message : null);
+		}
+
+	}
+
+	class NumberDividerFactory{	
+
+		private $dividers = [];
+
+		function __construct(){
+			array_push($this->dividers, new NumberDivider(3, 'Linio'));
+			array_push($this->dividers, new NumberDivider(5, 'IT'));
+			array_push($this->dividers, new NumberDivider(15, 'Linianos'));
+		}
+
+		public function validation($number){
+			$message = '';
+			foreach($this->dividers as $divider){
+				$value = $divider->validation($number);
+				$message = is_null($value) ? $message : $value;
+			}
+
+			echo $number.' = '.$message.PHP_EOL;
+		}
+
+	}
+
+	class Challenge{
+		
+		function __construct(){		
+			$numberFactory = new NumberDividerFactory();
+
+			foreach(range(1, 100) as $number){				
+				$numberFactory->validation($number);
+			}
+		}
+
+	}
+
+	new Challenge();
+?>
